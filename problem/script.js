be62008fc7b08e9132126f4c29faff3de3026c4b
al119c <example@gmail.com>
2021-01-29 22:58:01 +0300

    exersice 3 M

--- problem/script.js
+++ problem/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const num = prompt("Enter year", "from 1 to 2999");
+const num = +prompt("Enter year", "from 1 to 2999");
 
 function checkIsLeap(year) {
   if (year % 4 == 0) {
@@ -10,7 +10,7 @@ function checkIsLeap(year) {
   }
 }
 
-if (num < 1 || num > 2999) {
+if (num < 1 || num > 2999 || isNaN(num)) {
   alert('Out of range');
 } else {
   checkIsLeap(num);
