3a441b6cd6236b62b9cd208efe105baf92e3b295
al119c <example@gmail.com>
2021-10-03 23:03:56 +0300

    feat: implement functions

--- task/script.js
+++ task/script.js
@@ -1,288 +1,67 @@
-let arr = [
-  "affenpinscher",
-  "whippet",
-  "african",
-  "irish wolfhound",
-  "pembroke",
-  "airedale",
-  "NEWFOUNDLAND",
-  "OTTERHOUND",
-  "PAPILLON",
-  "PEKINESE",
-  "SWISS MOUNTAIN",
-  "weimaraner",
-  "MINIATURE PINSCHER",
-  "akita",
-  "GERMAN POINTER",
-  "vizsla",
-  "POMERANIAN",
-  "appenzeller",
-  "MINIATURE POODLE",
-  "yorkshire terrier",
-  "STANDARD POODLE",
-  "basenji",
-  "BERNESE MOUNTAIN",
-  "wheaten terrier",
-  "TOY POODLE",
-  "beagle",
-  "MEXICANHAIRLESS",
-  "westhighland terrier",
-  "PUG",
-  "bluetick",
-  "TIBETAN MASTIFF",
-  "toy terrier",
-  "PYRENEES",
-  "borzoi",
-  "BULL MASTIFF",
-  "tibetan terrier",
-  "REDBONE",
-  "bouvier",
-  "MALTESE",
-  "silky terrier",
-  "CHESAPEAKE RETRIEVER",
-  "boxer",
-  "MALINOIS",
-  "sealyham terrier",
-  "CURLY RETRIEVER",
-  "brabancon",
-  "MALAMUTE",
-  "scottish terrier",
-  "FLATCOATED RETRIEVER",
-  "briard",
-  "LHASA",
-  "patterdale terrier",
-  "GOLDEN RETRIEVER",
-  "boston bulldog",
-  "LEONBERG",
-  "norwich terrier",
-  "RHODESIAN RIDGEBACK",
-  "french bulldog",
-  "LABRADOR",
-  "norfolk terrier",
-  "ROTTWEILER",
-  "staffordshire bullterrier",
-  "KUVASZ",
-  "lakeland terrier",
-  "SALUKI",
-  "cairn",
-  "KOMONDOR",
-  "kerryblue terrier",
-  "SAMOYED",
-  "chihuahua",
-  "KELPIE",
-  "irish terrier",
-  "SCHIPPERKE",
-  "chow",
-  "KEESHOND",
-  "fox terrier",
-  "GIANT SCHNAUZER",
-  "clumber",
-  "HUSKY",
-  "dandie terrier",
-  "MINIATURE SCHNAUZER",
-  "border collie",
-  "WALKER HOUND",
-  "border terrier",
-  "ENGLISH SETTER",
-  "coonhound",
-  "ENGLISH HOUND",
-  "bedlington terrier",
-  "GORDON SETTER",
-  "cardigan corgi",
-  "BLOOD HOUND",
-  "australian terrier",
-  "IRISH SETTER",
-  "dachshund",
-  "BASSET HOUND",
-  "american terrier",
-  "ENGLISH SHEEPDOG",
-  "great dane",
-  "AFGHAN HOUND",
-  "stbernard",
-  "SHETLAND SHEEPDOG",
-  "scottish deerhound",
-  "IBIZAN HOUND",
-  "english springer",
-  "SHIBA",
-  "dhole",
-  "GROENENDAEL",
-  "welsh spaniel",
-  "SHIHTZU",
-  "dingo",
-  "ITALIAN GREYHOUND",
-  "sussex spaniel",
-  "BLENHEIM SPANIEL",
-  "doberman",
-  "GERMANSHEPHERD",
-  "japanese spaniel",
-  "BRITTANY SPANIEL",
-  "norwegian elkhound",
-  "ESKIMO",
-  "irish spaniel",
-  "COCKER SPANIEL",
-  "entlebucher",
-  "ENTLEBUCHER",
-  "cocker spaniel",
-  "IRISH SPANIEL",
-  "eskimo",
-  "NORWEGIAN ELKHOUND",
-  "brittany spaniel",
-  "JAPANESE SPANIEL",
-  "germanshepherd",
-  "DOBERMAN",
-  "blenheim spaniel",
-  "SUSSEX SPANIEL",
-  "italian greyhound",
-  "DINGO",
-  "shihtzu",
-  "WELSH SPANIEL",
-  "groenendael",
-  "DHOLE",
-  "shiba",
-  "ENGLISH SPRINGER",
-  "Ibizan hound",
-  "SCOTTISH DEERHOUND",
-  "shetland sheepdog",
-  "STBERNARD",
-  "afghan hound",
-  "GREAT DANE",
-  "english sheepdog",
-  "AMERICAN TERRIER",
-  "basset hound",
-  "DACHSHUND",
-  "irish setter",
-  "AUSTRALIAN TERRIER",
-  "blood hound",
-  "CARDIGAN CORGI",
-  "gordon setter",
-  "BEDLINGTON TERRIER",
-  "english hound",
-  "COONHOUND",
-  "english setter",
-  "BORDER TERRIER",
-  "walker hound",
-  "BORDER COLLIE",
-  "miniature schnauzer",
-  "DANDIE TERRIER",
-  "husky",
-  "CLUMBER",
-  "giant schnauzer",
-  "FOX TERRIER",
-  "keeshond",
-  "CHOW",
-  "schipperke",
-  "IRISH TERRIER",
-  "kelpie",
-  "CHIHUAHUA",
-  "samoyed",
-  "KERRYBLUE TERRIER",
-  "komondor",
-  "CAIRN",
-  "saluki",
-  "LAKELAND TERRIER",
-  "kuvasz",
-  "STAFFORDSHIRE BULLTERRIER",
-  "rottweiler",
-  "NORFOLK TERRIER",
-  "labrador",
-  "FRENCH BULLDOG",
-  "rhodesian ridgeback",
-  "NORWICH TERRIER",
-  "leonberg",
-  "BOSTON BULLDOG",
-  "golden retriever",
-  "PATTERDALE TERRIER",
-  "lhasa",
-  "BRIARD",
-  "flatcoated retriever",
-  "SCOTTISH TERRIER",
-  "malamute",
-  "BRABANCON",
-  "curly retriever",
-  "SEALYHAM TERRIER",
-  "malinois",
-  "BOXER",
-  "chesapeake retriever",
-  "SILKY TERRIER",
-  "maltese",
-  "BOUVIER",
-  "redbone",
-  "TIBETAN TERRIER",
-  "bull mastiff",
-  "BORZOI",
-  "pyrenees",
-  "TOY TERRIER",
-  "tibetan mastiff",
-  "BLUETICK",
-  "pug",
-  "WESTHIGHLAND TERRIER",
-  "mexicanhairless",
-  "BEAGLE",
-  "toy poodle",
-  "WHEATEN TERRIER",
-  "bernese mountain",
-  "BASENJI",
-  "standard poodle",
-  "YORKSHIRE TERRIER",
-  "miniature poodle",
-  "APPENZELLER",
-  "pomeranian",
-  "VIZSLA",
-  "german pointer",
-  "AKITA",
-  "miniature pinscher",
-  "WEIMARANER",
-  "swiss mountain",
-  "AIREDALE",
-  "pekinese",
-  "WHIPPET",
-  "papillon",
-  "AFRICAN",
-  "otterhound",
-  "IRISH WOLFHOUND",
-  "newfoundland",
-  "PEMBROKE",
-  "AFFENPINSCHER"
-];
-const num = 24;
-const txt = 'ren';
+let input = document.querySelector('.input'),
+  checkbox = document.querySelector('.case'),
+  lgth = document.querySelector('.length'),
+  substring = document.querySelector('.substring'),
+  output = document.querySelector('.output');
 
-let input = document.getElementsByClassName('input'),
-  checkbox = document.getElementsByClassName('case'),
-  lgth = document.getElementsByClassName('length'),
-  substring = document.getElementsByClassName('substring'),
-  output = document.getElementsByClassName('output');
+let num = 20;
 
-// functions
+async function getData() {
+  let response = await fetch('https://api.allorigins.win/raw?url=https://www.mrsoft.by/data.json');
+  let responseObj = await response.json();
+  let data = await responseObj.data;
+  return data;
+}
 
-const checkLength = (e) => {
-  let checked = [];
-  for (let i = 0; i < e.length; i++) {
-    if (e[i].length > num) {
-      checked.push(e[i]);
+let x = getData();
+x.then(value => {
+ 
+  lgth.addEventListener('click', () => {
+    output.innerHTML = '';
+    
+    let checked = [];
+    
+    if (input.value === '' ||  input.value === undefined ) {
+      output.innerHTML = `<div class="text">wrong input data</div>`;
+    } else {
+      for (let i = 0; i < value.length; i++) {
+        if (value[i].length > input.value) {
+          checked.push(value[i]);
+        }
+      }
     }
-  }
-  return checked;
-};
+    
+    checked.forEach(e => {
+      output.innerHTML += `<div class="text">${e}</div>`;
+    });
+  });
+  
+  substring.addEventListener('click', () => {
+    output.innerHTML = '';
+    
+    let result = [];
 
-const sensitiveSearch = (e) => {
-  let result = [];
-  for (let i = 0; i < e.length; i++) {
-    if (e[i].toLowerCase().includes(txt.toLowerCase())) {
-      result.push(e[i]);
+    if (input.value === '' ||  input.value === undefined ) {
+      output.innerHTML = `<div class="text">wrong input data</div>`;
+    } else  {
+
+      if (checkbox.checked) {
+        for (let i = 0; i < value.length; i++) {
+          if (value[i].includes(input.value)) {
+            result.push(value[i]);
+          }
+        }
+      
+      } else {
+        for (let i = 0; i < value.length; i++) {
+          if (value[i].toLowerCase().includes(input.value.toLowerCase())) {
+          result.push(value[i]);
+          }
+        }
+      }
     }
-  }
-  return result;
-};
-
-const insensitiveSearch = (e) => {
-  let result = [];
-  for (let i = 0; i < e.length; i++) {
-    if (e[i].includes(txt)) {
-      result.push(e[i]);
-    }
-  }
-  return result;
-};
-
-console.log(sensitiveSearch(arr));
+    result.forEach(e => {
+      output.innerHTML += `<div class="text">${e}</div>`;
+    });
+  });
+});
